01087394aaf66d300dcaed7f99e337da08f47b33
Mohammed Ibrahim <mohammedi@cpp.edu>
2022-03-18 18:54:11 -0700

    add more skeleton code and comments

--- src/Withdrawal.java
+++ src/Withdrawal.java
@@ -17,9 +17,31 @@ public class Withdrawal extends Transaction
        check to ensure that not too much money is withdrawn from the account. It will return true if the
        withdrawal is successful and false if it was not.
      */
-    public boolean withdrawFrom(int sourceAccountID, BigDecimal amount){
+/*    public boolean withdrawFrom(int sourceAccountID, BigDecimal amount)
+    {
         boolean withdrawSuccessful = false;
         return withdrawSuccessful;
     }
+*/
 
+    public boolean availableBalanceInAccount()
+    {
+        //check if the withdrawal is possible, else return false
+        return true;
+    }
+
+    public BigDecimal withdrawCash(BigDecimal amount)
+    {
+        BigDecimal dispensedAmount;
+        //dispensedAmount = amount to withdraw;
+        //update amount in the bank account (probably not here though);
+
+        return dispensedAmount;
+    }
+
+    public boolean checkWithdrawalAmount()
+    {
+        // return true if withdrawal amount is within the withdrawal limit, else return false
+        return true;
+    }
 }
