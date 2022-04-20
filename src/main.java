c762f836b48e678a6dfe962d1bf9788ff13b749d
ShaneBaxter831 <smbaxter@cpp.edu>
2022-04-20 14:11:47 -0700

    added database schema, added module to connect java to MySQL, created an example of how to bring data from the databse to the java program in the main method

--- src/main.java
+++ src/main.java
@@ -1,7 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class main
 {
     public static void main(String[] args)
     {
-        System.out.println("test");
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "Sjkh83lasd87ds0por7Gjjd6l4");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM member");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstName"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
