955a367d22dd07e5a04b17c8f4bb560e577af801
Daniel Rodriguez <40216857+drodriguez97@users.noreply.github.com>
2022-05-08 08:29:17 -0700

    adding WithdrawalSceneController Skeleton

---
+++ src/WithdrawalSceneController.java
@@ -0,0 +1,73 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WithdrawalSceneController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button sevenButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button withdrawButton;
+
+    @FXML
+    private Label withdrawalValueLabel;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void processNumber(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void withdraw(ActionEvent event) {
+
+    }
+
+}
