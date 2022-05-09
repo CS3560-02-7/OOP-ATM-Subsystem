06b556314840d08c6fdc1d001cb88c538751ca76
Abdur <61475787+abdur10567@users.noreply.github.com>
2022-05-08 17:20:44 -0700

    Added imports

--- src/Withdrawal.java
+++ src/Withdrawal.java
@@ -5,7 +5,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 
 public class Withdrawal extends Transaction
 {
